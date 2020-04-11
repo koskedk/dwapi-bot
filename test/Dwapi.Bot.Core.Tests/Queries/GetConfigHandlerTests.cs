3bcfd5b4afcf00c40cd79e91a76df4a75fe3ecf8
Koske Kimutai <koskedk@live.com>
2020-04-11 20:26:45 +0300

    adding config query

---
+++ test/Dwapi.Bot.Core.Tests/Queries/GetConfigHandlerTests.cs
@@ -0,0 +1,40 @@
+using Dwapi.Bot.Core.Application.Configs.Queries;
+using Dwapi.Bot.Core.Application.Indices.Commands;
+using Dwapi.Bot.Infrastructure;
+using MediatR;
+using System.Linq;
+using Microsoft.Extensions.DependencyInjection;
+using NUnit.Framework;
+using Serilog;
+
+namespace Dwapi.Bot.Core.Tests.Queries
+{
+    [TestFixture]
+    public class GetConfigHandlerTests
+    {
+        private IMediator _mediator;
+        [OneTimeSetUp]
+        public void Init()
+        {
+            TestInitializer.ClearDb();
+        }
+
+        [SetUp]
+        public void SetUp()
+        {
+            _mediator = TestInitializer.ServiceProvider.GetService<IMediator>();
+        }
+
+        [Test]
+        public void should_Refresh()
+        {
+            var getConfig = new GetConfig();
+
+            var result = _mediator.Send(getConfig).Result;
+            Assert.True(result.IsSuccess);
+            Assert.True(result.Value.Any());
+            Log.Debug($"{result.Value.Count()} Records");
+        }
+    }
+}
+
---
+++ test/Dwapi.Bot.Infrastructure.Tests/Data/MatchConfigRepositoryTests.cs
@@ -0,0 +1,38 @@
+using System.Linq;
+using Dapper;
+using Dwapi.Bot.Core.Domain.Configs;
+using Dwapi.Bot.Core.Domain.Indices;
+using Dwapi.Bot.Infrastructure.Tests.TestArtifacts;
+using Dwapi.Bot.SharedKernel.Enums;
+using Microsoft.Extensions.DependencyInjection;
+using NUnit.Framework;
+using Serilog;
+
+namespace Dwapi.Bot.Infrastructure.Tests.Data
+{
+    [TestFixture]
+    public class MatchConfigRepositoryTests
+    {
+        private IMatchConfigRepository _repository;
+
+        [OneTimeSetUp]
+        public void Init()
+        {
+            TestInitializer.ClearDb();
+            TestInitializer.SeedData(TestData.GenerateSubjects(true));
+        }
+
+        [SetUp]
+        public void SetUp()
+        {
+            _repository = TestInitializer.ServiceProvider.GetService<IMatchConfigRepository>();
+        }
+        [Test,Order(1)]
+        public void should_Get_All()
+        {
+            var configs = _repository.GetConfigs().ToList();
+            Assert.True(configs.Any());
+            Log.Debug($"{configs.Count} Records");
+        }
+ }
+}
